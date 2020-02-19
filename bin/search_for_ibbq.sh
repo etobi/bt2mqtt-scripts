59eaeff06e4c2f46eb56463626dd0af879dc87da
Tobias Liebig <work@etobi.de>
2020-02-19 21:12:49 +0100

    start only one ibbq.sh instance

--- bin/search_for_ibbq.sh
+++ bin/search_for_ibbq.sh
@@ -20,6 +20,13 @@ echo -e 'devices\nexit\n' >&${COPROC[1]}
 IFS= read -d '' output <&${COPROC[0]}
 ibbq=$(echo -e "$output" | egrep "^Device" | grep iBBQ | cut -c 8-24)
 
+pgrep ibbq.sh
+exit_status=$?
+if [[ ${exit_status} -eq 0 ]]; then
+  echo "already running"
+  exit
+fi
+
 if [[ "$ibbq" != "" ]]; then
   echo "start ibbq"
   ./action/ibbq.sh "$mqtthost" "$ibbq"
